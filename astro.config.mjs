6e8fbe33986e3efccb26e998aa069a28fd637d8a
ExolutionTech <exolutionza@gmail.com>
2024-10-07 00:37:06 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon(), tailwind()],
-  site: 'https://exolutionza.github.io',
-  base: 'webcraft-astro',
+  site: 'https://webcraft.co.za',
 });
